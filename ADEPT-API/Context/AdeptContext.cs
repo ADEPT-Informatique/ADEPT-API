4306c11e8148e3c45d3dd6901bc07e427814697a
OIivier guy <olivier.guy@hotmail.com>
2021-10-04 20:39:44 -0400

    Fixes

--- ADEPT-API/Context/AdeptContext.cs
+++ ADEPT-API/Context/AdeptContext.cs
@@ -1,24 +1,19 @@
 ﻿using ADEPT_API.Models;
-using ADEPT_API.Models.MembreConfiance;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ADEPT_API.Context
 {
     public class AdeptContext : DbContext
     {
 
-        public AdeptContext(DbContextOptions<AdeptContext> options): base(options)
+        public AdeptContext(DbContextOptions<AdeptContext> options) : base(options)
         {
 
         }
 
         public DbSet<User> Users { get; set; }
 
-        public DbSet<UserRole> UserRoles { get; set; }     
+        public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
