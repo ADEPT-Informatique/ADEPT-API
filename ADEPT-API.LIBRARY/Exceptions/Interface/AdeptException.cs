b79b97b88e9e128239e37b6eb97e09ad05635954
OIivier guy <olivier.guy@hotmail.com>
2021-10-17 15:04:48 -0400

    Fixing code smell first part

--- ADEPT-API.LIBRARY/Exceptions/Interface/AdeptException.cs
+++ ADEPT-API.LIBRARY/Exceptions/Interface/AdeptException.cs
@@ -5,12 +5,12 @@ namespace ADEPT_API.LIBRARY.Exceptions.Interface
 {
     public abstract class AdeptException : Exception
     {
-        protected AdeptException(string pErrorCode, string pMessage, HttpStatusCode? pHttpStatus) : base(pMessage)
+        protected AdeptException(string errorCode, string message, HttpStatusCode? httpStatus) : base(message)
         {
-            _ = string.IsNullOrWhiteSpace(pErrorCode) ? throw new ArgumentNullException(nameof(pErrorCode)) : string.Empty;
+            _ = string.IsNullOrWhiteSpace(errorCode) ? throw new ArgumentNullException(nameof(errorCode)) : string.Empty;
 
-            this.ErrorCode = !string.IsNullOrWhiteSpace(pErrorCode) ? pErrorCode : throw new ArgumentNullException($"{nameof(AdeptException)} was expecting a value for {nameof(pErrorCode)} but null or empty was provided");
-            this.HttpStatus = pHttpStatus;
+            this.ErrorCode = !string.IsNullOrWhiteSpace(errorCode) ? errorCode : throw new ArgumentNullException(nameof(errorCode), $"{nameof(AdeptException)} was expecting a value for {nameof(errorCode)} but null or empty was provided");
+            this.HttpStatus = httpStatus;
         }
 
         public string ErrorCode { get; set; }
--- ADEPT-API/Startup.cs
+++ ADEPT-API/Startup.cs
@@ -65,7 +65,7 @@ namespace ADEPT_API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async Task Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
